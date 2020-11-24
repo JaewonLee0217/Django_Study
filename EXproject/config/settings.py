ec2b7614aaa6533bcc1e560c41c85bf6975b87e9
jaewon Lee <jcircle67@gmail.com>
2020-11-24 23:27:24 +0900

    study
    study

--- EXproject/config/settings.py
+++ EXproject/config/settings.py
@@ -69,16 +69,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'knowhow',
+        'USER': 'root',
+        'PASSWORD': '12345!',
+        'HOST': '223.194.46.212',
+        'PORT': '3306',
     }
 }
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
--- djangogirls/mysite/blog/models.py
+++ djangogirls/mysite/blog/models.py
@@ -3,19 +3,19 @@ from django.db import models
 from django.utils import timezone
 # Django의 이러한 파일에서 코드들을 불러와라.
 
-class Post(models.Model): # Post라는 객체를 모델 정의.
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
-    #__STR__함수 호출하면 Post클래스로 찍은 객체의 이름을 알수 있음.
+# class Post(models.Model): # Post라는 객체를 모델 정의.
+#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=200)
+#     text = models.TextField()
+#     created_date = models.DateTimeField(
+#             default=timezone.now)
+#     published_date = models.DateTimeField(
+#             blank=True, null=True)
+#
+#     def publish(self):
+#         self.published_date = timezone.now()
+#         self.save()
+#
+#     def __str__(self):
+#         return self.title
+#     #__STR__함수 호출하면 Post클래스로 찍은 객체의 이름을 알수 있음.
